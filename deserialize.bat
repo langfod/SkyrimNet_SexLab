c78fb05358dc8c20ada1d1d46828c4c009d0f477
langfod <langfod@gmail.com>
2025-07-18 01:35:38 -0400

    Fixes deserilized.bat xcopy junk
    still need to handle this separately ....

--- deserialize.bat
+++ deserialize.bat
@@ -4,9 +4,11 @@ IF exist "dist" (
     mkdir dist
 )
 
-xcopy /s /d /i /EXCLUDE:exclusions.txt fomod dist
-xcopy /s /d /i /EXCLUDE:exclusions.txt Scripts dist
-xcopy /s /d /i /EXCLUDE:exclusions.txt SKSE dist
-xcopy /s /d /i /EXCLUDE:exclusions.txt SkyrimNet_Sexlab dist
+xcopy  fomod dist\fomod\ /S /I
+xcopy  Scripts dist\Scripts\ /S /I
+xcopy  SKSE dist\SKSE\ /S /I
+xcopy  SkyrimNet_SexLab\info.json dist\SkyrimNet_SexLab\ /S /I
+xcopy  SkyrimNet_SexLab\group_tags.json dist\SkyrimNet_SexLab\ /S /I
+
 
 SpriggitCLI\Spriggit.CLI.exe convert-to-plugin -i "Spriggit\SkyrimNet_Sexlab" -o "dist\SkyrimNet_Sexlab.esp" 
