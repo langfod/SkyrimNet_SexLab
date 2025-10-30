b88ccce35eb8da6105539051cab29dd869b3b0ca
langfod <langfod@gmail.com>
2025-10-30 04:27:22 -0400

    update workflow and deserialize
    - workflow uses it's own copy and serialize section instead of deseriallize.bat

--- deserialize.bat
+++ deserialize.bat
@@ -7,8 +7,8 @@ IF exist "dist" (
 xcopy  fomod dist\fomod\ /S /I
 xcopy  Scripts dist\Scripts\ /S /I
 xcopy  SKSE dist\SKSE\ /S /I
-xcopy  SkyrimNet_SexLab\info.json dist\SkyrimNet_SexLab\ /S /I
-xcopy  SkyrimNet_SexLab\group_tags.json dist\SkyrimNet_SexLab\ /S /I
+xcopy  SkyrimNet_SexLab dist\SkyrimNet_SexLab\ /S /I
+
 
 
 SpriggitCLI\Spriggit.CLI.exe convert-to-plugin -i "Spriggit\SkyrimNet_Sexlab" -o "dist\SkyrimNet_Sexlab.esp" 
